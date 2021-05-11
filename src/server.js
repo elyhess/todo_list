f137a6c457f4a80e0285a679dd80f0e5e986fb9a
Ely Hess <ely.hess@me.com>
2021-05-11 11:42:52 -0600

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import "dotenv/config"
+import express from "express";
+import routes from "./routes";
+
+const app = express();
+app.use(express.json());
+
+// app.use("/resources", routes.user);
+
+app.use((req, res) => {
+    res.status(404).send("404:Page not found");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+})
